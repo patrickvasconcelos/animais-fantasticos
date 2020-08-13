1007627a05927f85481f2f7ccd146630b0819251
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-08-13 00:17:06 -0300

    [NEW] Animação do scroll

--- script.js
+++ script.js
@@ -70,3 +70,27 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll(".js-scroll");
+
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop =
+                    section.getBoundingClientRect().top - windowMetade;
+
+                if (sectionTop < 0) {
+                    section.classList.add("ativo");
+                }
+            });
+        }
+
+        animaScroll();
+        window.addEventListener("scroll", animaScroll);
+    }
+}
+
+initAnimacaoScroll();
