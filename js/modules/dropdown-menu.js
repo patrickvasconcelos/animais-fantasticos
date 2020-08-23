288e169831d9a58519315d4541c59021c6f50e81
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-08-23 14:18:30 -0300

    [NEW] dropdown menu com js

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,1 +1,20 @@
-export default function initDropdownMenu() {}
+import outsideClick from "./outsideClick.js";
+
+export default function initDropdownMenu() {
+    const dropdownMenu = document.querySelectorAll("[data-dropdown]");
+    if (dropdownMenu) {
+        dropdownMenu.forEach((menu) => {
+            ["click", "touchstart"].forEach((userEvent) => {
+                menu.addEventListener(userEvent, handleClick);
+            });
+        });
+
+        function handleClick(event) {
+            event.preventDefault();
+            this.classList.add("ativo");
+            outsideClick(this, ["click", "touchstart"], () => {
+                this.classList.remove("ativo");
+            });
+        }
+    }
+}
