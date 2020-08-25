0fa624235c5d2b06d324e0037ff1150c08469652
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-08-25 00:11:20 -0300

    [CHG] menu hamburger

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,24 @@
+import outsideClick from "./outsideClick.js";
+
+export default function initMenuMobile() {
+    const menuButton = document.querySelector('[data-menu="button"]');
+    const menuButtonHamburger = document.querySelector(".contorno-menu");
+    const menuList = document.querySelector('[data-menu="list"]');
+    const eventos = ["click", "touchstart"];
+
+    if (menuButton) {
+        function openMenu(event) {
+            menuList.classList.add("active");
+            menuButton.classList.add("active");
+            menuButtonHamburger.classList.add("active");
+            outsideClick(menuList, eventos, () => {
+                menuList.classList.remove("active");
+                menuButton.classList.remove("active");
+                menuButtonHamburger.classList.remove("active");
+            });
+        }
+        eventos.forEach((evento) =>
+            menuButton.addEventListener(evento, openMenu)
+        );
+    }
+}
