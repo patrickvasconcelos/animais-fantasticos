dafbcdb23907d5be0458a3f5f616548349dd57b5
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-09-06 14:09:57 -0300

    [CHG] correção do caminho relativo

--- js/script.js
+++ js/script.js
@@ -49,5 +49,5 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]');
 funcionamento.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
