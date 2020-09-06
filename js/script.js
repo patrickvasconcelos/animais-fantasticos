b2fd24c1250a4b73c9e6b249657d1e53976f7de8
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-09-06 11:22:25 -0300

    [NEW] refatoração do tooltip.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
+/* eslint-disable comma-dangle */
 import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tab-nav.js';
 import Modal from './modules/modal.js';
-
-import initAnimacaoScroll from './modules/scroll-animacao.js';
-
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -31,10 +30,12 @@ const modal = new Modal(
 );
 modal.init();
 
-initAnimacaoScroll();
-initTooltip();
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimacaoScroll();
